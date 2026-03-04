b25f879a97bd2781683455a94214860e6925781c
Fess <fessbespalov@gmail.com>
2026-03-04 13:40:33 +0700

    feat: migrate Contracts.tsx to @stacks/connect v8 request() API, remove Asigna

--- src/Contracts.tsx
+++ src/Contracts.tsx
@@ -1,46 +1,35 @@
 import React from 'react';
-import { openContractCall, openContractDeploy, openSTXTransfer } from '@stacks/connect';
-import { StacksMainnet, StacksTestnet } from '@stacks/network';
-import { AnchorMode,BytesReader, deserializeTransaction, broadcastTransaction, PostConditionMode, noneCV, principalCV, uintCV, makeUnsignedSTXTokenTransfer, makeUnsignedContractCall } from '@stacks/transactions';
+import { request } from '@stacks/connect';
+import { noneCV, principalCV, uintCV } from '@stacks/transactions';
 import { useAddress, useNetwork } from '../atoms';
-import { bytesToHex } from '@stacks/common';
-import { useAsignaConnect, useAsignaOptions } from '@asigna/stx-connect';
-import { Buffer } from 'buffer';
-// import {useAsignaOptions, useAsignaConnect} from './asigna';
-// CONTRACT_CALL
 
 const Contracts = () => {
-    const stacksNetwork = useNetwork() === 'mainnet' ? new StacksMainnet() : new StacksTestnet();
+    const network = useNetwork();
     const [address] = useAddress();
-    
+
     const transferOptions = {
         recipient: 'SP31PNYDZ8Q86B47BYW67YD9PVXVSZ3JRJAN9DJDK',
         amount: '2000',
-        network: new StacksMainnet(),
-        anchorMode: AnchorMode.Any,
-        fee: 2222,
-    }
+        network,
+    };
+
     const functionArgs = [
         uintCV(100),
         principalCV(address),
         principalCV('SP31PNYDZ8Q86B47BYW67YD9PVXVSZ3JRJAN9DJDK'),
         noneCV(),
     ];
+
     const callOptions = {
-        network: new StacksMainnet(),
-        anchorMode: AnchorMode.Any,
-        contractAddress: 'SP351ZJK9F5AWZJEEZGZ7NZKSRKA65EWFRR48V9KS',
-        contractName: 'ALL2',
+        contract: 'SP351ZJK9F5AWZJEEZGZ7NZKSRKA65EWFRR48V9KS.ALL2' as `${string}.${string}`,
         functionName: 'transfer',
         functionArgs,
-        postConditionMode: PostConditionMode.Allow,
-        postConditions: [],
-        fee: 70000,
-    }
-    
+        network,
+    };
+
     const deployOptions = {
-        contractName: 'arkadiko-token',
-        codeBody: `
+        name: 'arkadiko-token',
+        clarityCode: `
 (impl-trait 'SP3FBR2AGK5H9QBDH3EEN6DF8EK8JY7RX8QJ5SVTE.sip-010-trait-ft-standard.sip-010-trait)
 (impl-trait .arkadiko-dao-token-trait-v1.dao-token-trait)
 
@@ -52,54 +41,31 @@ const Contracts = () => {
 
 ;; errors
 (define-constant ERR-NOT-AUTHORIZED u1401)`,
-        anchorMode: AnchorMode.Any,
-        postConditionMode: PostConditionMode.Allow,
-        postConditions: [],
-        network: stacksNetwork,
-    }
+        network,
+    };
 
+    const handleSend = async () => {
+        await request('stx_transferStx', transferOptions);
+    };
 
-    const {openAsignaStxTokenTransfer, openAsignaContractCall, openAsignaDeployContract} = useAsignaConnect();
+    const handleCall = async () => {
+        await request('stx_callContract', callOptions);
+    };
 
+    const handleDeploy = async () => {
+        await request('stx_deployContract', deployOptions);
+    };
 
-    const handleSend = async (withModal?: boolean) => {
-        if (withModal) {
-            const got = await openAsignaStxTokenTransfer(transferOptions, {});
-        } else {
-            openSTXTransfer(transferOptions);
-        }
-    }
-
-    const handleCall = async (withModal?: boolean) => {
-        if (withModal) {
-            const res = await openAsignaContractCall(callOptions, {});
-        } else {
-            openContractCall(callOptions);
-        }
-    }
-
-    const handleDeploy = async (withModal?: boolean) => {
-        if (withModal) {
-            const res = await openAsignaDeployContract(deployOptions, {});
-        } else {
-            openContractDeploy(deployOptions);
-        }
-    }
-
-    return <div style={{display: 'flex', gap: 18, fontWeight: 'bold', fontSize: 20}}>
-        <div>
-            <div>Raw actions</div>
-            <div onClick={() => handleCall()}>Call</div>
-            <div onClick={() => handleSend()}>Send</div>
-            <div onClick={() => handleDeploy()}>Deploy</div>
+    return (
+        <div style={{ display: 'flex', gap: 18, fontWeight: 'bold', fontSize: 20 }}>
+            <div>
+                <div>Actions</div>
+                <div onClick={handleCall} style={{ cursor: 'pointer' }}>Call</div>
+                <div onClick={handleSend} style={{ cursor: 'pointer' }}>Send</div>
+                <div onClick={handleDeploy} style={{ cursor: 'pointer' }}>Deploy</div>
+            </div>
         </div>
-        <div>
-            <div>Modal actions</div>
-            <div onClick={() => handleCall(true)}>Call</div>
-            <div onClick={() => handleSend(true)}>Send</div>
-            <div onClick={() => handleDeploy(true)}>Deploy</div>
-        </div>
-    </div>
-}
+    );
+};
 
 export default Contracts;
